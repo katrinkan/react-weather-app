71b5606247b1224b009eaa117ca3b480663cb823
katrinkan <96773609+katrinkan@users.noreply.github.com>
2022-03-21 10:02:00 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>Weather App</h2>
+        <footer>
+          <p>This project was coded by Katrin Kanape and is</p>
+          <a href="https://github.com/katrinkan/react-weather-app">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
